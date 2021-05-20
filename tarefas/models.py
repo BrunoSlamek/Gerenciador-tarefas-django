2ae70c1ebacefae1d614c668181b937cf62dc691
Bruno Slamek <brunoslamek2@gmail.com>
2021-05-20 13:18:01 -0300

    edit model

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+class Tarefa(models.Model):
+    OPCOES_STATUS = (
+        ('concluído', 'Concluído'),
+        ('pendente', 'Pendente'),
+        ('adiado', 'Adiado')
+    )
+
+    OPCOES_CATEGORIAS = (
+        ('urgente', 'Urgente'),
+        ('importante', "Importante"),
+        ('atencao', 'Atenção'),
+    )
+
+    descricao = models.CharField(max_length=400)
+    criacao = models.DateTimeField(auto_now_add=True)
+    categoria = models.CharField(max_length=25, choices=OPCOES_CATEGORIAS,
+                                default='importante')
+    status = models.CharField(max_length=25, choices=OPCOES_STATUS,
+                            default='pendente')
